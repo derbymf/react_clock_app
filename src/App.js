8e5fc95cd66b825a3d9a2f8155c334578721e4bc
michael <derbymf@jmu.edu>
2019-04-04 21:09:59 -0400

    renamed project and formatted css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Clock from 'react-live-clock';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+           Time Zones
           </p>
-          <a
+
+        </header>
+        <div class="california">
+        <p>
+          Time in California
+        </p>
+          <Clock 
+                  format={'H:mm:ss'}
+                  ticking={true}
+                  timezone={'US/Pacific'}/>
+
+      </div>
+      <div class="colorado">
+      <p>
+            Time in Colorado
+        </p>
+          <Clock 
+                  format={'H:mm:ss'}
+                  ticking={true}
+                  timezone={'US/Mountain'}/>
+      </div>
+      <div class="newyork">
+      <p>
+      Time in New York
+          </p>
+          <Clock 
+                  format={'H:mm:ss'}
+                  ticking={true}
+                  timezone={'US/Eastern'}/>
+      </div>
+      <footer className="App-footer">
+      <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.timeanddate.com/time/map/
+            "
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            For More Information About Time Zones
           </a>
-        </header>
+      </footer>
       </div>
+
     );
   }
 }
